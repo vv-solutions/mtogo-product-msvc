7fc09e343f4807227c59a7d50bc93afd81e5a8ef
Vincent Buchholz <vbu@miracle.dk>
2023-11-12 09:08:53 +0100

    test without bdd

--- src/test/java/dk/vv/mtogo/product/msvc/pojos/NetPriceTest.java
+++ src/test/java/dk/vv/mtogo/product/msvc/pojos/NetPriceTest.java
@@ -1,9 +1,9 @@
-package dk.vv.mtogo.product.msvc.pojos;
-
-import io.quarkiverse.cucumber.CucumberQuarkusTest;
-
-public class NetPriceTest extends CucumberQuarkusTest {
-    public static void main(String[] args) {
-        runMain(NetPriceTest.class, args);
-    }
-}
+//package dk.vv.mtogo.product.msvc.pojos;
+//
+//import io.quarkiverse.cucumber.CucumberQuarkusTest;
+//
+//public class NetPriceTest extends CucumberQuarkusTest {
+//    public static void main(String[] args) {
+//        runMain(NetPriceTest.class, args);
+//    }
+//}
